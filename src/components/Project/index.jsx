4515a0a110790b11c818be9e2e494177b6365dc4
John Hinojosa <77072244+takolad@users.noreply.github.com>
2026-09-03 11:52:28 -0500

    enhance project grid

--- src/components/Project/index.jsx
+++ src/components/Project/index.jsx
@@ -10,9 +10,30 @@ function Project({ props }) {
       <Link className="card-img-top" to={linkTxt}>
         <img alt={altTxt} src={props.img} className="w-100"/>
       </Link>
+      {/* things to add: group/solo, repo link, conditional for heroku sites */}
       <div className="card-body">
-        <h5 class="card-title">{props.name}</h5>
+        <div className="d-flex justify-content-between">
+          <h5 class="card-title">{props.name}</h5>
+          {props.type === "solo" ? (
+            <i className="fas fa-male align-self-center"> Solo Project</i>
+          ) : (
+            <i className="fas fa-people-carry align-self-center"> Group Project</i>
+          )}
+        </div>
         <p className="card-text">{props.description}</p>
+        <div className="d-flex justify-content-end align-items-center gap-3">
+          {props.deploy_url.includes('heroku')? (
+            <i class="far fa-dizzy fs-3 text-danger" title="Previously hosted on Heroku"></i>
+          ) : (
+            <a href={props.deploy_url} target="_blank" className="btn btn-primary">
+            Visit Site
+          </a>
+          )}
+
+          <a href={props.repo_url} target="_blank" className="btn btn-secondary">
+            Visit <i class="fab fa-github"></i> Repo
+          </a>
+        </div>
       </div>
     </div>
   );
