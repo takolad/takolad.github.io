7ca51e5000166f7be5df9cbf4abebbe7228c26af
John Hinojosa <77072244+takolad@users.noreply.github.com>
2026-09-03 13:12:37 -0500

    fix attribute name, comment out link

--- src/components/Project/index.jsx
+++ src/components/Project/index.jsx
@@ -7,12 +7,12 @@ function Project({ props }) {
   let linkTxt = `/portfolio/${props.id}`;
   return (
     <div className="card mb-2">
-      <Link className="card-img-top" to={linkTxt}>
+      {/* <Link className="card-img-top" to={linkTxt}> */}
         <img alt={altTxt} src={props.img} className="w-100"/>
-      </Link>
+      {/* </Link> */}
       <div className="card-body">
         <div className="d-flex justify-content-between">
-          <h5 class="card-title">{props.name}</h5>
+          <h5 className="card-title">{props.name}</h5>
           {props.type === "solo" ? (
             <i className="fas fa-male align-self-center"> Solo Project</i>
           ) : (
@@ -22,7 +22,7 @@ function Project({ props }) {
         <p className="card-text">{props.description}</p>
         <div className="d-flex justify-content-end align-items-center gap-3">
           {props.deploy_url.includes('heroku')? (
-            <i class="far fa-dizzy fs-3 text-danger" title="Previously hosted on Heroku"></i>
+            <i className="far fa-dizzy fs-3 text-danger" title="Previously hosted on Heroku"></i>
           ) : (
             <a href={props.deploy_url} target="_blank" className="btn btn-primary">
             Visit Site
@@ -30,7 +30,7 @@ function Project({ props }) {
           )}
 
           <a href={props.repo_url} target="_blank" className="btn btn-secondary">
-            Visit <i class="fab fa-github"></i> Repo
+            Visit <i className="fab fa-github"></i> Repo
           </a>
         </div>
       </div>
